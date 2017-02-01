e689a56f8ba47c769f64b327a91b3bb279bd0697
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-01 12:01:29 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,28 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	//esto es un comentario de una línea
+	/*esto es un comentario  de más de una línea 
+	de largo */
+	var nombre;//defino variable
+	nombre= "Tanya"//cargo variable
+
+
+
+
+
+
+	nombre=prompt("Ingresa tu nombre","Natalia Natalia");
+
+
+
+
+
+
+
+
+	alert("Nombre:  "+nombre);
 	
+	var person = prompt("Ingresa tu nombre", "Natalia Natalia");
 }
 
