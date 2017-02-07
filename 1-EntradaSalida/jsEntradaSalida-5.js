3ee4c6b6ca28a235bfb65120832117ef49834152
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-07 10:58:43 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,17 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+	var nombre; //declaro las variables
+	var edad;
+
+	nombre=document.getElementById("elNombre").value; //uso esta forma de GET porque me pide tomar el nombre. Igual que ej. 3
+	edad=document.getElementById("laEdad").value; //para usar esta forma de tomar el dato se pone VALUE después del ()
+
+	alert("Usted se llama "+nombre+"  y su edad es "+edad); //los espacios van adentro de las comillas
+
+
 }
+
+
+
 
