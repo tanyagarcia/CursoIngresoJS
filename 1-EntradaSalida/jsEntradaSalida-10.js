366a58780a0f285187c022f2b98726bc21f929dd
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-08 09:43:21 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+	var resultado;
+
+	importe=document.getElementById('importe').value;
+
+	importe=parseInt(importe);
+
+	resultado=importe-(importe*0.25); //saco el 25 % de decuento que es 25 dividido 100
+
+	alert(resultado);
 }
