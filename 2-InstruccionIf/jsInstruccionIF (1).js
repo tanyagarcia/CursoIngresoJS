b5e2fe4cea24cd0b8f15474f609f7eb9edc9fbba
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-08 10:35:59 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad
+	var edad;
 
+	edad=document.getElementById('edad').value;
+
+	if (edad==15) //acá no va punto y coma
+	 {
+	 	alert("Niña bonita");           //acá con punto y coma
+	 }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad 
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if (edad>17)
+		{
+			alert("Es mayor de edad");
+		}
 
 
 
