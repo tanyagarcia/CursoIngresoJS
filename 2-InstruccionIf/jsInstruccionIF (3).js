2afead5b12cef7f214c30410ab52d042fb990f62
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-08 11:17:12 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad //declaro la variable
+
+	edad=document.getElementById('edad').value; //la cargo
+
+	if (edad>17) //acá averiguo si es mayor de 18
+		{
+			alert("Es mayor de edad");
+		}
+
+	if (edad<17)
+		{
+			alert("Es menor de edad"); //acá averiguo si es menor de 18
+		}
 
 
 }//FIN DE LA FUNCIÓN
