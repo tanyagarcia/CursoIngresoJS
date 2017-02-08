99d6403961798b83ba2cc26e34b2b908b9e9c943
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-08 09:21:17 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,15 @@ en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
 
-	
+	var sueldo;
+	var resultado;
+
+	sueldo=document.getElementById("sueldo").value;
+
+	sueldo=parseInt(sueldo);
+
+	resultado=sueldo*1.1; //sueldo+sueldo*0.10 =sueldo*1+0.10= sueldo*1.1 porque es el 10% (por ej si es el 90% es 0.9)
+
+	alert(resultado);
+
 }
