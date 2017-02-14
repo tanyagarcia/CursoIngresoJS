f1fab730ee41d3675e1bc70806c56907bb1ba9e3
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-14 09:19:12 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	//Genero el número RANDOM entre 1 y 10
+	var numeroRandom;
+
+	numeroRandom=Math.floor(Math.random() * 10) + 1;
+
+	alert("El número Random es "+numeroRandom);
 
 }//FIN DE LA FUNCIÓN
