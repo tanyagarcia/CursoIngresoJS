5d403ba2265dea99e4577f70a5ccf231fe938e97
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-14 11:35:17 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,30 @@
 function Mostrar()
 {
-//tomo la edad  
+
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+case "Enero":
+case "Febrero":
+case "Marzo":
+case "Abril":
+case "Mayo":
+case "Junio":
+	alert("Ya pasamos el frio, ahora calor");
+	break;
 
+case "Julio":
+case "Agosto":
+	alert("Abrigate que hace frío");
+	break;
 
-
+case "Septiembre":
+case "Octubre":
+case "Noviembre":
+case "Diciembre":
+	alert("Falta para el invierno");
+	break;
+}
 
 }//FIN DE LA FUNCIÓN
