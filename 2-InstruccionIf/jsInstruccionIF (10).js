17473bfc7d0a6a16d63bcf72fca60b0fed95ad66
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-14 10:32:53 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,23 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
 
+	numeroRandom=Math.floor(Math.random() * 10) + 1;
+
+	if(numeroRandom<4)
+	{
+		alert(numeroRandom+" Vamos la próxima se puede");
+	}
+	else
+	{
+		if(numeroRandom>3&&numeroRandom<9)
+		{
+			alert(numeroRandom+" Aprobó");
+		}
+		else
+		{
+			alert(numeroRandom+" Excelente");
+		}
+	}
 }//FIN DE LA FUNCIÓN
