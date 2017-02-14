2e9d466bb7c51e5a5fb8b9586f919f3ddc3ee164
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-14 11:41:26 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,9 +1,18 @@
 function Mostrar()
 {
-//tomo la edad  
+
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Febrero":
+		alert("Este mes no tiene más de 29 días");
+		break;
+
+	default:
+		alert("Este mes tiene 30 o más días");
+		
+}
 	
 	
 
