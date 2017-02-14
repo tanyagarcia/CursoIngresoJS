893855d24473f172e3c971b91c148d021a830274
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-14 11:07:22 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,25 @@
 function Mostrar()
 {
-//tomo la edad  
+ 
 var mesDelAño = document.getElementById('mes').value;
 
+switch(mesDelAño)
+{
+case "Enero":
+	alert("Que comiences bien el año");
+	break;
 
+case "Marzo":
+	alert("A clases");
+	break;
 
+case "Julio":
+	alert("Se vienen las vacaciones");
+	break;
+
+case "Diciembre":
+	alert("Felices Fiestas");
+	break;
+}
 
 }//FIN DE LA FUNCIÓN
