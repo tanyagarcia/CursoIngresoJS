b1a4e0452f860e2aa145f98e23f11a128b71701c
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-15 07:49:20 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -3,6 +3,25 @@ function Mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
+if(laHora)
+{
+	switch(laHora)
+	{
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+			alert("Es de mañana");
+			break;
+}
+else
+{
+		case: "12"
+			alert("Es mediodía");
+			break;
+}
 
+}
 
 }//FIN DE LA FUNCIÓN
