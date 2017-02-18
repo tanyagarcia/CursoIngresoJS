9644a486bd5b41c57c10e8ddba1685d68c183f31
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-18 12:59:44 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,22 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	var promedio;
+
+	while(contador<5) //porque tiene que hacer la suma 5 veces
+		{
+
+			contador++; //aumento el contador de a uno
+			numero=prompt("Ingrese datos"); //pido el dato por prompt
+			numero=parseInt(numero); //convierto el numero a entero
+			acumulador=acumulador+numero; //hago la fórmula del acumulador+numero porque tiene que ir sumando el numero ingresado
+
+		}
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('suma').value=acumulador; //para que me muestre el numero acumulado por document.get
+document.getElementById('promedio').value=acumulador/5; //para que me muestre el promedio por document.get
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,6 +4,17 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var seguir;
+	var dato;
+
+	while(respuesta=="si")
+		{
+			dato=prompt("Ingrese datos");
+			acumulador=acumulador+dato;
+
+			contador++;
+
+		}
 
 
 document.getElementById('suma').value=acumulador;
