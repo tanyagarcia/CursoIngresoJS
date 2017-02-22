72fad65f6ecebadf684ca0cc14ad04d08646678d
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-22 12:09:14 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 
+/* al generar numeros random del 0 al 10 se debe contar la cantidad de cada una de estas cifras, repetir la iteracion para lanzar el random 100.000 veces e informar 
+1) la cantidad de veces que salio cada numero del 0 al 10 
+2) el porcentaje de veces que salio cada numero con respecto al total
+
+for de cero a cien mil
+dentro del for numero random
+luego un switch con case contador de uno contador de uno mas uno. contador por cada numero
+
 
 
 
