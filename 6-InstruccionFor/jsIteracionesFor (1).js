26bdad8c1bc18b7a66434a2343b350eb70c80a0d
tanyagarcia <garcia.tanya0204@gmail.com>
2017-02-22 12:06:25 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,21 @@
 function Mostrar()
-{
+{	
+	/* var contador=0;
 
+	for(; ;)
+	{
+		if(contador==5)
+		{
+			break;
+		}
+		console.log(contador);
+		contador++;
+	}
+	*/
+
+
+		for(contador=1;contador<11;contador++)
+		{
+		console.log(contador);
+		}
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,8 @@
 function Mostrar()
 {
-
+	for(contador=10;contador<0;contador--)
+	{
+		console.log(contador);
+	}
 
 }
